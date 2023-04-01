62e5adbd5812ea6daa341272179fe1ec1a498f43
Emretester <emreyeni2019@gmail.com>
2023-04-01 12:24:29 +0300

    Project Finished

---
+++ src/test/java/com/centrilli/runners/FailedTestRunner.java
@@ -0,0 +1,12 @@
+package com.centrilli.runners;
+
+import io.cucumber.junit.*;
+import org.junit.runner.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "cydeo/step_definitions",
+        features = "@target/rerun.txt"
+)
+public class FailedTestRunner {
+}
